55fd6f57cec6fbc9bc44653bd42625e472f9ddc7
Stefan Niederhauser <stefan.niederhauser@mimacom.com>
2014-01-13 19:53:46 +0100

    allow to find out issue of segment

--- src/main/java/stni/atlassian/remote/query/JavaFacade.java
+++ src/main/java/stni/atlassian/remote/query/JavaFacade.java
@@ -1,6 +1,8 @@
 package stni.atlassian.remote.query;
 
+import scala.Option;
 import stni.text.transform.Parser;
+import stni.text.transform.Segment;
 
 /**
  *
@@ -19,6 +21,11 @@ public class JavaFacade {
 
     public static QueryParser throwingQueryParser(Parser parser) {
         return new QueryParser(parser, NoOpPreProcessor$.MODULE$, ThrowingParseExceptionHandler$.MODULE$);
+    }
+
+    public static QueryIssueList issueOf(Segment segment) {
+        Option<QueryIssueList> issue = QueryParser.issueOf(segment);
+        return issue.isEmpty() ? null : issue.get();
     }
 
 }
--- src/main/scala/stni/atlassian/remote/query/QueryParser.scala
+++ src/main/scala/stni/atlassian/remote/query/QueryParser.scala
@@ -6,15 +6,20 @@ import stni.text.transform.{Attribute, Segment, Parser}
  *
  */
 class QueryParser(parser: Parser, preProcessor: PreProcessor = NoOpPreProcessor, exceptionHandler: ParseExceptionHandler = ThrowingParseExceptionHandler) {
-  private val ISSUE = Attribute("issue")
 
   def parse(input: String, issue: QueryIssueList): Segment = {
     try {
-      parser.parse(preProcessor.preProcess(input))(ISSUE -> issue)
+      parser.parse(preProcessor.preProcess(input))(QueryParser.ISSUE -> issue)
     } catch {
       case e: Exception => exceptionHandler.handleException(input, issue, e)
     }
   }
+}
+
+object QueryParser {
+  private val ISSUE = Attribute[QueryIssueList]("issue")
+
+  def issueOf(segment: Segment): Option[QueryIssueList] = segment.inherited(ISSUE)
 }
 
 trait PreProcessor {
